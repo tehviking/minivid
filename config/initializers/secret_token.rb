bd791b753d182253c1ea87c7e7b27560e3835e5f
tehviking <brandon.hays@gmail.com>
2010-09-01 22:05:10 -0600

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minivid::Application.config.secret_token = '4a96b96f70613edd67e5bf832777100fa7371a9fd85401bc3b0507aa1de167cd5f5b91b88102ca2b15aae17b8f03efc9b15aa8c201adfc27203051e868ddbf83'
