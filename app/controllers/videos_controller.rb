1fc513c8f5632900196683d3e6cf5eca55df9355
tehviking <brandon.hays@gmail.com>
2010-09-15 20:17:52 -0600

    trying new video profile

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,16 +2,24 @@ class VideosController < ApplicationController
   def show
     @video = Video.find(params[:id])
     @original_video = @video.panda_video
-    @h264_encoding = @original_video.encodings.find_by_profile_name("h264")
+    @h264_encoding = @original_video.encodings.find_by_profile_name("h264noresize")
   end
 
   def new
     @video = Video.new
-    profile = Panda::Profile.create(:preset_name => "60sec")
+    profile = Panda::Profile.create()
   end
 
   def create
     @video = Video.create!(params[:video])
     redirect_to :action => :show, :id => @video.id 
+  end
+  
+  def destroy
+    @video = Video.find(params[:id])
+    panda_video = Panda::Video.find(@video.panda_video_id)
+    panda_video.delete
+    @video.destroy
+    redirect_to :videos_path
   end
 end
